e7275a5f76b2cd0f5cec664c803547c8e70c96d9
Alexandr2111 <alivanov358@gmail.com>
2023-08-30 20:30:31 +0300

    - completed commit

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -1,5 +1,6 @@
 package com.tictactoe;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,30 +10,77 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
-
 @WebServlet(name = "LogicServlet", value = "/logic")
 public class LogicServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Получаем текущую сессию
-        HttpSession currentSession = req.getSession();
-        // Получаем объект игрового поля из сессии
-        Field field = extractField(currentSession);
-        // получаем индекс ячейки, по которой произошел клик
-        int index = getSelectedIndex(req);
 
-        // ставим крестик в ячейке, по которой кликнул пользователь
+        HttpSession currentSession = req.getSession();
+
+        Field field = extractField(currentSession);
+
+        int index = getSelectedIndex(req);
+        Sign currentSign = field.getField().get(index);
+
+        if (Sign.EMPTY != currentSign) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+            dispatcher.forward(req, resp);
+            return;
+        }
+
         field.getField().put(index, Sign.CROSS);
 
-        // Считаем список значков
+        if (checkWin(resp, currentSession, field)) {
+            return;
+        }
+
+        int emptyFieldIndex = field.getEmptyFieldIndex();
+        if (emptyFieldIndex >= 0) {
+            field.getField().put(emptyFieldIndex, Sign.NOUGHT);
+
+            if (checkWin(resp, currentSession, field)) {
+                return;
+            }
+        }
+        else {
+            currentSession.setAttribute("draw", true);
+
+            List<Sign> data = field.getFieldData();
+            currentSession.setAttribute("data", data);
+
+            resp.sendRedirect("/index.jsp");
+            return;
+        }
+
         List<Sign> data = field.getFieldData();
 
-        // Обновляем объект поля и список значков в сессии
         currentSession.setAttribute("data", data);
         currentSession.setAttribute("field", field);
 
         resp.sendRedirect("/index.jsp");
+    }
 
+    /**
+     * Метод проверяет, нет ли трех крестиков/ноликов в ряд.
+     */
+    private boolean checkWin(HttpServletResponse response, HttpSession currentSession, Field field) throws IOException {
+        Sign winner = field.checkWin();
+        if (Sign.CROSS == winner || Sign.NOUGHT == winner) {
+
+            currentSession.setAttribute("winner", winner);
+
+            List<Sign> data = field.getFieldData();
+            currentSession.setAttribute("data", data);
+            response.sendRedirect("/index.jsp");
+            return true;
+        }
+        return false;
+    }
+
+    private int getSelectedIndex(HttpServletRequest request) {
+        String click = request.getParameter("click");
+        boolean isNumeric = click.chars().allMatch(Character::isDigit);
+        return isNumeric ? Integer.parseInt(click) : 0;
     }
 
     private Field extractField(HttpSession currentSession) {
@@ -43,12 +91,6 @@ public class LogicServlet extends HttpServlet {
         }
         return (Field) fieldAttribute;
     }
-
-
-    private int getSelectedIndex(HttpServletRequest request) {
-        String click = request.getParameter("click");
-        boolean isNumeric = click.chars().allMatch(Character::isDigit);
-        return isNumeric ? Integer.parseInt(click) : 0;
-    }
-
 }
+
+
